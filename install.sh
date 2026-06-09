cad1fe6ab43960390e73d040fb83cab73874dc8d
Nafees Hassan <nafeeshassan16@gmail.com>
2026-06-09 11:39:09 +0500

    sway -> hyprland

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ mkdir $HOME/.config/htop/
 mkdir $HOME/.config/i3/
 mkdir $HOME/.config/mpv/
 mkdir $HOME/.config/sway/
+mkdir $HOME/.config/hypr/
 mkdir $HOME/.config/waybar/
 mkdir $HOME/.config/mako/
 mkdir $HOME/.config/wireplumber
 mkdir $HOME/.config/wireplumber/wireplumber.conf.d
-mkdir $HOME/.omnisharp
 
 ln -s $PWD/alacritty.toml $HOME/.config/alacritty/alacritty.toml
 ln -s $PWD/htoprc $HOME/.config/htop/htoprc
@@ -18,6 +18,7 @@ ln -s $PWD/i3status $HOME/.config/i3/i3status
 ln -s $PWD/mpv.conf $HOME/.config/mpv/mpv.conf
 ln -s $PWD/nvim/ $HOME/.config/nvim
 ln -s $PWD/swayconfig $HOME/.config/sway/config
+ln -s $PWD/hyprland.conf $HOME/.config/hypr/hyprland.conf
 ln -s $PWD/bins/ $HOME/.local/bin
 ln -s $PWD/bash/aliases $HOME/.bash_aliases
 ln -s $PWD/bash/profile $HOME/.bash_profile
@@ -29,4 +30,3 @@ ln -s $PWD/waybar/style.css $HOME/.config/waybar/style.css
 ln -s $PWD/makoconfig $HOME/.config/mako/config
 ln -s $PWD/wireplumber-nocamera.conf \
 	$HOME/.config/wireplumber/wireplumber.conf.d/10-disable-camera.conf
-ln -s $PWD/omnisharp.json $HOME/.omnisharp/omnisharp.json
